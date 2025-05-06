a9311aefe7cf110c4fca305edb0066afaa502807
🅰️🅻🅴🆇 🎮 <demian.x.mas@gmail.com>
2025-05-06 15:20:51 +0300

    Coin manager
    The game has a new system "Coin Manager". We will actively work on it so that over time it becomes better, more convenient and optimized for each user.

---
+++ Assets/GAME/Own scripts/Scripts/Coins/UICoinManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine;
+
+public class UICoinManager : MonoBehaviour
+{
+    public static UICoinManager Instance; // Singleton instance to ensure only one UI manager exists
+    
+    public TextMeshProUGUI CoinText; // Reference to the "TextMeshPro" UI element displaying the coin count
+
+    private void Awake()
+    {
+        // Ensure only one instance exists
+        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        
+    }
+
+    public void UpdateCoinText(int coinCount) // Updates the "coin" text UI
+    {
+        if (CoinText != null) // Ensure text component exists
+        {
+            CoinText.text = coinCount.ToString(); // Convert int to string
+        }
+    }
+}
