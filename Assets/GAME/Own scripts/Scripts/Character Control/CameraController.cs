91b3d8dadad2f4eadbcaf7eb6b6ae7d298abaad7
🅰️🅻🅴🆇 🎮 <demian.x.mas@gmail.com>
2025-05-12 16:02:54 +0300

    Refactored and more readable version with improved structure, separation of logic
    improved structure and separation of logic

--- Assets/GAME/Own scripts/Scripts/Character Control/CameraController.cs
+++ Assets/GAME/Own scripts/Scripts/Character Control/CameraController.cs
@@ -5,128 +5,144 @@ namespace Synty.AnimationBaseLocomotion.Samples
 {
     public class CameraController : MonoBehaviour
     {
+       
         private const int _LAG_DELTA_TIME_ADJUSTMENT = 20;
 
+       
+        [Header("Character & Camera References")]
         [Tooltip("The character game object")]
-        [SerializeField]
-        private GameObject _syntyCharacter;
-        
+        [SerializeField] private GameObject _syntyCharacter;
         [Tooltip("Main camera used for player perspective")]
-        [SerializeField]
-        private Camera _mainCamera;
+        [SerializeField] private Camera _mainCamera;
 
-        [SerializeField]
-        private Transform _playerTarget;
-        [SerializeField]
-        private Transform _lockOnTarget;
+        [Header("Target Transforms")]
+        [SerializeField] private Transform _playerTarget;
+        [SerializeField] private Transform _lockOnTarget;
 
-        [SerializeField]
-        private bool _invertCamera;
-        [SerializeField]
-        private bool _hideCursor;
-        [SerializeField]
-        private bool _isLockedOn;
-        [SerializeField]
-        private float _mouseSensitivity ;
-        [SerializeField]
-        private float _cameraDistance ;
-        [SerializeField]
-        private float _cameraHeightOffset;
-        [SerializeField]
-        private float _cameraHorizontalOffset;
-        [SerializeField]
-        private float _cameraTiltOffset;
-        [SerializeField]
-        private Vector2 _cameraTiltBounds = new Vector2(-10f, 45f);
-        [SerializeField]
-        private float _positionalCameraLag = 1f;
-        [SerializeField]
-        private float _rotationalCameraLag = 1f;
+        [Header("Input & Control Settings")]
+        [SerializeField] private bool _invertCamera;
+        [SerializeField] private bool _hideCursor;
+        [SerializeField] private bool _isLockedOn;
+
+        [Header("Camera Movement Settings")]
+        [SerializeField] private float _mouseSensitivity;
+        [SerializeField] private float _cameraDistance;
+        [SerializeField] private float _cameraHeightOffset;
+        [SerializeField] private float _cameraHorizontalOffset;
+        [SerializeField] private float _cameraTiltOffset;
+        [SerializeField] private Vector2 _cameraTiltBounds = new Vector2(-10f, 45f);
+        [SerializeField] private float _positionalCameraLag = 1f;
+        [SerializeField] private float _rotationalCameraLag = 1f;
+
+        
         private float _cameraInversion;
-
         private InputReader _inputReader;
-        private float _lastAngleX;
-        private float _lastAngleY;
-
-        private Vector3 _lastPosition;
-
-        private float _newAngleX;
-
-        private float _newAngleY;
-        private Vector3 _newPosition;
-        private float _rotationX;
-        private float _rotationY;
-
         private Transform _syntyCamera;
 
-        
+        private float _lastAngleX, _lastAngleY;
+        private float _newAngleX, _newAngleY;
+        private float _rotationX, _rotationY;
+
+        private Vector3 _lastPosition, _newPosition;
+
+      
         private void Start()
         {
-            _syntyCamera = gameObject.transform.GetChild(0);
+            InitializeReferences();
+            SetupCursor();
+            InitializeCamera();
+            SyncInitialCameraTransform();
+        }
 
+        private void Update()
+        {
+            UpdateRotation();
+            UpdatePosition();
+            UpdateCameraTransform();
+            SavePreviousState();
+        }
+
+       
+        private void InitializeReferences()
+        {
+            _syntyCamera = transform.GetChild(0);
             _inputReader = _syntyCharacter.GetComponent<InputReader>();
             _playerTarget = _syntyCharacter.transform.Find("SyntyPlayer_LookAt");
             _lockOnTarget = _syntyCharacter.transform.Find("TargetLockOnPos");
+        }
 
+        private void SetupCursor()
+        {
             if (_hideCursor)
             {
                 Cursor.visible = false;
                 Cursor.lockState = CursorLockMode.Locked;
             }
+        }
 
+        private void InitializeCamera()
+        {
             _cameraInversion = _invertCamera ? 1 : -1;
+        }
 
+        private void SyncInitialCameraTransform()
+        {
             transform.position = _playerTarget.position;
             transform.rotation = _playerTarget.rotation;
-
             _lastPosition = transform.position;
 
-            _syntyCamera.localPosition = new Vector3(_cameraHorizontalOffset, _cameraHeightOffset, _cameraDistance * -1);
+            _syntyCamera.localPosition = new Vector3(_cameraHorizontalOffset, _cameraHeightOffset, -_cameraDistance);
             _syntyCamera.localEulerAngles = new Vector3(_cameraTiltOffset, 0f, 0f);
         }
 
-        
-        private void Update()
+       
+        private void UpdateRotation()
         {
-            float positionalFollowSpeed = 1 / (_positionalCameraLag / _LAG_DELTA_TIME_ADJUSTMENT);
-            float rotationalFollowSpeed = 1 / (_rotationalCameraLag / _LAG_DELTA_TIME_ADJUSTMENT);
+            float rotationSpeed = 1 / (_rotationalCameraLag / _LAG_DELTA_TIME_ADJUSTMENT);
 
             _rotationX = _inputReader._mouseDelta.y * _cameraInversion * _mouseSensitivity;
-
             _rotationY = _inputReader._mouseDelta.x * _mouseSensitivity;
 
             _newAngleX += _rotationX;
             _newAngleX = Mathf.Clamp(_newAngleX, _cameraTiltBounds.x, _cameraTiltBounds.y);
-            _newAngleX = Mathf.Lerp(_lastAngleX, _newAngleX, rotationalFollowSpeed * Time.deltaTime);
+            _newAngleX = Mathf.Lerp(_lastAngleX, _newAngleX, rotationSpeed * Time.deltaTime);
 
             if (_isLockedOn)
             {
                 Vector3 aimVector = _lockOnTarget.position - _playerTarget.position;
                 Quaternion targetRotation = Quaternion.LookRotation(aimVector);
-                targetRotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationalFollowSpeed * Time.deltaTime);
+                targetRotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
                 _newAngleY = targetRotation.eulerAngles.y;
             }
             else
             {
                 _newAngleY += _rotationY;
-                _newAngleY = Mathf.Lerp(_lastAngleY, _newAngleY, rotationalFollowSpeed * Time.deltaTime);
+                _newAngleY = Mathf.Lerp(_lastAngleY, _newAngleY, rotationSpeed * Time.deltaTime);
             }
+        }
 
-            _newPosition = _playerTarget.position;
-            _newPosition = Vector3.Lerp(_lastPosition, _newPosition, positionalFollowSpeed * Time.deltaTime);
-
+        private void UpdatePosition()
+        {
+            float positionSpeed = 1 / (_positionalCameraLag / _LAG_DELTA_TIME_ADJUSTMENT);
+            _newPosition = Vector3.Lerp(_lastPosition, _playerTarget.position, positionSpeed * Time.deltaTime);
             transform.position = _newPosition;
+        }
+
+        private void UpdateCameraTransform()
+        {
             transform.eulerAngles = new Vector3(_newAngleX, _newAngleY, 0);
-
-            _syntyCamera.localPosition = new Vector3(_cameraHorizontalOffset, _cameraHeightOffset, _cameraDistance * -1);
+            _syntyCamera.localPosition = new Vector3(_cameraHorizontalOffset, _cameraHeightOffset, -_cameraDistance);
             _syntyCamera.localEulerAngles = new Vector3(_cameraTiltOffset, 0f, 0f);
+        }
 
+        private void SavePreviousState()
+        {
             _lastPosition = _newPosition;
             _lastAngleX = _newAngleX;
             _lastAngleY = _newAngleY;
         }
 
-       
+        
         public void LockOn(bool enable, Transform newLockOnTarget)
         {
             _isLockedOn = enable;
@@ -137,45 +153,14 @@ namespace Synty.AnimationBaseLocomotion.Samples
             }
         }
 
-       
-        public Vector3 GetCameraPosition()
-        {
-            return _mainCamera.transform.position;
-        }
-
-       
-        public Vector3 GetCameraForward()
-        {
-            return _mainCamera.transform.forward;
-        }
-
-       
-        public Vector3 GetCameraForwardZeroedY()
-        {
-            return new Vector3(_mainCamera.transform.forward.x, 0, _mainCamera.transform.forward.z);
-        }
-
-        
-        public Vector3 GetCameraForwardZeroedYNormalised()
-        {
-            return GetCameraForwardZeroedY().normalized;
-        }
-        
-        
-        public Vector3 GetCameraRightZeroedY()
-        {
-            return new Vector3(_mainCamera.transform.right.x, 0, _mainCamera.transform.right.z);
-        }
-        
-        public Vector3 GetCameraRightZeroedYNormalised()
-        {
-            return GetCameraRightZeroedY().normalized;
-        }
-
-        
-        public float GetCameraTiltX()
-        {
-            return _mainCamera.transform.eulerAngles.x;
-        }
+        #region Camera Info Getters
+        public Vector3 GetCameraPosition() => _mainCamera.transform.position;
+        public Vector3 GetCameraForward() => _mainCamera.transform.forward;
+        public Vector3 GetCameraForwardZeroedY() => new Vector3(_mainCamera.transform.forward.x, 0, _mainCamera.transform.forward.z);
+        public Vector3 GetCameraForwardZeroedYNormalised() => GetCameraForwardZeroedY().normalized;
+        public Vector3 GetCameraRightZeroedY() => new Vector3(_mainCamera.transform.right.x, 0, _mainCamera.transform.right.z);
+        public Vector3 GetCameraRightZeroedYNormalised() => GetCameraRightZeroedY().normalized;
+        public float GetCameraTiltX() => _mainCamera.transform.eulerAngles.x;
+        #endregion
     }
 }
